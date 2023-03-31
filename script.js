474283be988d930db8b044f159fa68c934ca5f7c
funkysys <aadelbos@gmail.com>
2023-03-31 23:36:30 +0200

    upgrade for chrome and firefox

--- script.js
+++ script.js
@@ -1,7 +1,20 @@
-console.log(gsap)
 const card = document.querySelectorAll(".card")
+const contentCard = document.querySelectorAll(".content-card")
+const glow = document.querySelectorAll(".glow")
+
+window.addEventListener('load', () => {
+gsap.to(card, {opacity: 1, top: 0, left: 0, rotation:360, ease:Linear.easeNone,duration: 1})
+gsap.to(contentCard,{opacity: 1, top: 0, left: 0,rotation:360, ease:Linear.easeNone,duration: 1})
+gsap.to(glow, {opacity: 1, top: 0, left: 0,rotation:360, ease:Linear.easeNone,duration: 1})
+})
+
+
+let angleX
+let angleY
 
 card.forEach( el => {
+	console.log(el.children[0]);
+	console.log(el[1]);
 	el.addEventListener("mousemove", e => {
 
 		let elRect = el.getBoundingClientRect()
@@ -12,8 +25,8 @@ card.forEach( el => {
 		let midCardWidth = elRect.width / 2
 		let midCardHeight = elRect.height / 2
 
-		let angleY = -(x - midCardWidth) / 8
-		let angleX = (y - midCardHeight) / 8
+		angleY = -(x - midCardWidth) / 8
+		angleX = (y - midCardHeight) / 8
 
 		el.children[0].style.transform = `rotateX(${angleX}deg) rotateY(${angleY}deg) scale(1.1)`
 		el.children[1].style.transform = `rotateX(${angleX}deg) rotateY(${angleY}deg) scale(1.1)`
@@ -21,8 +34,8 @@ card.forEach( el => {
 	
 	el.addEventListener("mouseleave", () => {
 
-		gsap.to(el.children[0], {rotateX: 0, rotateY: 0, duration: 1})
-		gsap.to(el.children[1], {rotateX: 0, rotateY: 0, duration: 1})
+		gsap.to(contentCard, {rotateX: 0 , rotateY: 0, duration: 1})
+		gsap.to(glow, {rotateX: 0, rotateY: 0, duration: 1})
 		// el.children[0].style.transform = "rotateX(0) rotateY(0)"
 		// el.children[1].style.transform = "rotateX(0) rotateY(0)"
 	})
